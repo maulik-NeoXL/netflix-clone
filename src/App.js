9dbf323e57b630cfdfadc015a3d1645e0c87453f
maulik-NeoXL <maulik@neoxl.com>
2023-05-24 22:41:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import Navbar from "./components/Navbar";
+import {Routes, Route} from "react-router-dom";
+import Home from './pages/Home'
 
 function App() {
   return (
-    <div className="App">
-      <p className='font-bold text-white'> Hi </p>
+    <div>
       <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+      </Routes>
     </div>
+
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
 
 export default function Navbar(){
   return(
-    <div>
-      
+    <div className='flex items-center justify-between p-4 z-[100] w-full absolute'>
+      <h1 className='text-[#E50914] text-4xl font-bold cursor-pointer '> NETLFIX</h1>
+      <div className=''>
+        <button className='text-white pr-4'> Sign In</button>
+        <button className='bg-[#E50915] px-6 py-2 text-white font-bold rounded cursor-pointer'> Sign Up</button>
+      </div>
     </div>
   )
 }
