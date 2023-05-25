55e45b4f80cfed4c97398870f431b6e39c742b6a
maulik-NeoXL <maulik@neoxl.com>
2023-05-25 22:49:14 +0530

    first commit

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -6,7 +6,7 @@ export default function Movie({item}){
   const [like, setLike] = useState(false)
 
   return(
-    <div className='w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative pl-4 mt-2'>
+    <div className='w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative pl-8 mt-2'>
       <img className='w-full h-auto block' src={`https://image.tmdb.org/t/p/original/${item?.backdrop_path}`} alt={item.title} />
       <div className='absolute top-0 left-0 w-full h-full opacity-0 hover:bg-black/80 hover:opacity-100 text-white'>
         <p className='white-space-normal text-sx md:text-s font-bold flex justify-center items-center h-full text-center'> {item?.title}</p>
--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -3,7 +3,7 @@ import axios from 'axios'
 import {MdChevronLeft, MdChevronRight} from 'react-icons/md';
 import Movie from "./Movie";
 
-export default function Row({title, fetchURL}){
+export default function Row({title, fetchURL, rowID}){
 
   const [movies, setMovies] = useState([])
 
@@ -14,19 +14,33 @@ export default function Row({title, fetchURL}){
       })
   }, [fetchURL]);
 
-  console.log(movies);
+  const slideLeft = () =>{
+    var slider = document.getElementById('slider' + rowID)
+    slider.scrollLeft = slider.scrollLeft - 1000;
+  }
+
+  const slideRight = () =>{
+      var slider = document.getElementById('slider' + rowID)
+      slider.scrollLeft = slider.scrollLeft + 1000;
+    }
 
   return(
     <>
-        <h2 className='text-white font-bold md:text-xl pl-8 mt-4'> {title}</h2>
-      <div className='relative items-center flex'>
-        <MdChevronLeft size={40} className='bg-white rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden '/>
-               <div id={'slider'} className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide'>
+        <h2
+          className='text-white font-bold md:text-xl pl-8 mt-4'> {title}</h2>
+      <div
+        className='relative items-center flex group'>
+        <MdChevronLeft
+          onClick={slideLeft}
+          size={40}
+          className='bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block '/>
+               <div id={'slider' + rowID}
+                 className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide'>
                  {movies.map((item, id) => (
                    <Movie item={item} key={id}/>
                    ))}
                </div>
-        <MdChevronLeft size={40} className='bg-white rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden '/>
+        <MdChevronRight onClick={slideRight} size={40} className='bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 group-hover:block hidden '/>
       </div>
     </>
   )
