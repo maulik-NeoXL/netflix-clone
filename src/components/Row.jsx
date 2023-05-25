d26945c0a86268f703c09c4e7228ab56ebef6f61
maulik-NeoXL <maulik@neoxl.com>
2023-05-25 21:42:11 +0530

    first commit

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from "react";
 import axios from 'axios'
+import {MdChevronLeft, MdChevronRight} from 'react-icons/md';
 import Movie from "./Movie";
 
 export default function Row({title, fetchURL}){
@@ -19,11 +20,13 @@ export default function Row({title, fetchURL}){
     <>
         <h2 className='text-white font-bold md:text-xl pl-8 mt-4'> {title}</h2>
       <div className='relative items-center flex'>
+        <MdChevronLeft size={40} className='bg-white rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden '/>
                <div id={'slider'} className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide'>
                  {movies.map((item, id) => (
                    <Movie item={item} key={id}/>
                    ))}
                </div>
+        <MdChevronLeft size={40} className='bg-white rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden '/>
       </div>
     </>
   )
