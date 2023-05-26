85e7a052bb15d2308d196afe515fdfc276eceddc
maulik-NeoXL <maulik@neoxl.com>
2023-05-26 10:02:58 +0530

    first commit

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -27,10 +27,10 @@ export default function Main (){
   return(
     <div className='w-full h-[550px] text-white'>
       <div className='w-full h-full'>
-        <div className='absolute w-full h-[550px] bg-gradient-to-r from-black'></div>
+        <div className='absolute w-full h-[550px] md:bg-gradient-to-r from-black '></div>
         <img className='w-full h-full object-cover' src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`} alt={movie?.title} />
         <div className='absolute w-full top-[25%] p-4 md: p-8'>
-          <h1 className='text-4xl md: text-6xl font-bold'> {movie?.title}</h1>
+          <h1 className='text-2xl md:text-6xl font-bold'> {movie?.title}</h1>
           <div className='my-4'>
             <button className='border border-gray-300 text-black bg-gray-300 py-2 px-5'> Play Now</button>
             <button className='border border-gray-300 text-white bg-black py-2 px-5 ml-4'> Watch Later</button>
