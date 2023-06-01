8101f203e020cf01eee3a28fbfc48492ebd0f512
maulik-NeoXL <maulik@neoxl.com>
2023-06-01 12:58:55 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAT4kc6msKJI_-RvZzTogB2bWc9Dl56nzc",
+  authDomain: "netflix-react-clone-b190b.firebaseapp.com",
+  projectId: "netflix-react-clone-b190b",
+  storageBucket: "netflix-react-clone-b190b.appspot.com",
+  messagingSenderId: "1002591955839",
+  appId: "1:1002591955839:web:b7232373a40b40315cc84a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
